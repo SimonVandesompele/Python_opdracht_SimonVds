7a42d29813ea65e6067c1b3b5a31f9e926e774cc
SimonVandesompele <115065783+SimonVandesompele@users.noreply.github.com>
2023-12-30 22:14:59 +0100

    Update database
    create table en 2 nieuwe methodes toegevoegd aan databas.py

--- app/db/database.py
+++ app/db/database.py
@@ -17,6 +17,15 @@ class Database:
             )
         ''')
 
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS zoos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                location TEXT NOT NULL
+            )
+        ''')
+
+
     def add_animal(self, animal):
         self.cursor.execute('''
             INSERT INTO animals (name, species)
@@ -27,6 +36,18 @@ class Database:
     def get_all_animals(self):
         self.cursor.execute('SELECT * FROM animals')
         return self.cursor.fetchall()
+
+    def add_zoo(self, zoo):
+        self.cursor.execute('''
+            INSERT INTO zoos (name, location)
+            VALUES (?, ?)
+        ''', (zoo.name, zoo.location))
+        self.connetion.commit()
+
+    def get_all_zoos(self):
+        self.cursor.execute('SELECT * FROM zoos')
+        return self.cursor.fetchall()
+
 
     def close_connection(self):
         self.connetion.close()
